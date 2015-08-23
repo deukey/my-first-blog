d20068d481d16728be0e6c97a4fe8bf4db26ec89
deukey <deukey@naver.com>
2015-08-23 09:24:41 +0900

    Modified settings.py for temp.

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import sys #temp
+import sitecustomize #temp
+
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -102,3 +105,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+print >> sys.stderr, "sitecustomize is being loaded from %s" % (sitecustomize.__file__,) #temp
