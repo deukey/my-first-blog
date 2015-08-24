594304ff3fb504765e4342337f837c2062231928
deukey <deukey@naver.com>
2015-08-24 15:30:24 +0900

    Added views to create/edit blog post inside the site.

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,9 +12,6 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-import sys #temp
-import sitecustomize #temp
-
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -105,5 +102,3 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-print >> sys.stderr, "sitecustomize is being loaded from %s" % (sitecustomize.__file__,) #temp
